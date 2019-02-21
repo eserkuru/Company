83a868a90880051449f97d3ef059b8270bb885c7
Eser Kuru <eser.kuru@infoline-tr.com>
2019-02-21 18:00:05 +0300

    AdminLTE was implemented.

---
+++ Company.Manager.WebCoreUI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Company.Manager.WebCoreUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
